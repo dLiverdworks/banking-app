39245529ac0b3804430c7fd8aa3495dd6da1f289
DESKTOP-BDE2T1R\mhuss <mhussuduri@gmail.com>
2024-12-17 13:49:16 +0700

    Initial commit

---
+++ src/main/java/com/javaguides/banking/mapper/AccountMapper.java
@@ -0,0 +1,24 @@
+package com.javaguides.banking.mapper;
+
+import com.javaguides.banking.dto.AccountDto;
+import com.javaguides.banking.entity.Account;
+
+public class AccountMapper {
+
+    public static Account mapToAccount(AccountDto accountDto){
+        return new Account(
+                accountDto.getId(),
+                accountDto.getAccountHolderName(),
+                accountDto.getBalance()
+        );
+    }
+
+    public static AccountDto mapToAccountDto(Account account){
+        return new AccountDto(
+                account.getId(),
+                account.getAccountHolderName(),
+                account.getBalance()
+        );
+    }
+
+}
